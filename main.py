1254cb85fa378f1e6c06b1081d2672bce2a225d2
Rabi Raj Khadka <er.rabirajkhadka@gmail.com>
2018-01-19 18:19:02 +0545

    building model final phase

---
+++ main.py
@@ -0,0 +1,18 @@
+import urllib
+import zipfile
+import nottingham_util
+import rnn
+
+#collect the data from server
+url="http://www-etud.iro.umontreal.ca/~boulanni/Nottingham.zip"
+urllib.urlretrieve(url,'dataset.zip')
+
+#read zip file and extract it to data folder
+zip = zipfile.ZipFile(r'dataset.zip')
+zip.extractall('data')
+
+#build the model
+nottingham_util.create_model()
+
+#train the model created
+rnn.train_model()
