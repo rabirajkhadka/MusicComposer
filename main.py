b5ca3a61ae9684b91d1a4207148014e7d66cdd50
Rabi Raj Khadka <er.rabirajkhadka@gmail.com>
2018-02-02 11:11:11 +0545

    Internet Dependency Removed

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import nottingham_util
 import rnn
 
 #collect the data from server
-url="http://www-etud.iro.umontreal.ca/~boulanni/Nottingham.zip"
-urllib.urlretrieve(url,'dataset.zip')
+#url="http://www-etud.iro.umontreal.ca/~boulanni/Nottingham.zip"
+#urllib.urlretrieve(url,'dataset.zip')
 
 #read zip file and extract it to data folder
 zip = zipfile.ZipFile(r'dataset.zip')
